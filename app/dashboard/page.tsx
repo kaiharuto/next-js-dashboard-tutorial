1757014e98dd671b52445ee850a2e5a6615b1460
= <=>
2026-02-12 20:39:53 +0100

    Starting Chapter 7

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+
 export default function Page() {
-  return <p>Dashboard Page</p>;
+  <main>
+    <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      Dashboard
+    </h1>
+    <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"></div>
+  </main>;
+  // return <p>Dashboard Page</p>;
 }
