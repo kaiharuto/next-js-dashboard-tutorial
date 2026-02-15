34e69c5dee849a5cf429258ea4798863dfedfc9f
= <=>
2026-02-15 17:23:11 +0100

    Finished Chapter 10

---
+++ app/lib/actions.ts
@@ -0,0 +1,70 @@
+"use server";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: require });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  // It's usually good practice to store monetary values in cents in your database
+  // to eliminate JavaScript floating-point errors and ensure greater accuracy.
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    insert into invoices (customer_id, amount,status,date)
+  values (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    update invoices
+    set customer_id = ${customerId},
+    amount = ${amountInCents},
+    status = ${status},
+    date = ${date}
+    where id=${id}
+    `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`
+    delete from invoices
+    where id=${id}
+    `;
+
+  revalidatePath("/dashboard/invoices");
+}
